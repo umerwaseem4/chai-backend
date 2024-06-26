0141e9bf05e08f16cf6214931ef79127b897fd92
Umerwaseem4 <umerwas4@gmail.com>
2024-06-26 23:53:42 +0500

    added file upload

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -62,6 +62,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 };
 
+// generating token (access + refresh)
 userSchema.methods.generateAccessToken = function () {
     return jwt.sign(
         {
