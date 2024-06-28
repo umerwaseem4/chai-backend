5141cee6f09fa45152c63f4f77b0a0b85d3de47f
Umerwaseem4 <umerwas4@gmail.com>
2024-06-28 05:02:04 +0500

    fixed few lines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -30,7 +30,16 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // uploading images to cloudinary
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImg[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImg[0]?.path;
+
+    let coverImageLocalPath;
+    if (
+        req.files &&
+        Array.isArray(req.files.coverImg) &&
+        req.files.coverImg.length > 0
+    ) {
+        coverImageLocalPath = req.files.coverImg[0].path;
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, 'Avatar file is required!');
